c4652b4470195d4f25104ef9d2423efbc73e0317
kauwers <kauwers@umich.edu>
2017-02-09 14:43:45 -0500

    part 2 finished

--- proj2.py
+++ proj2.py
@@ -1,7 +1,4 @@
 #proj2.py
-
-
-#### Problem 1 ####
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import ssl, urllib.error, urllib.parse
@@ -11,15 +8,13 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
+#### Problem 1 ####
 print('\n*********** PROBLEM 1 ***********')
 print('New York Times -- First 10 Story Headings\n')
 
 ### Your Problem 1 solution goes here
-
 siteHandle = "http://nytimes.com"
-
 html=urlopen(siteHandle, context=ctx).read()
-
 soup = BeautifulSoup(html, 'html.parser')
 
 count = 0
@@ -28,9 +23,7 @@ for header in soup.find_all('h2'):
         if header.has_attr('class') and header['class'][0] == 'story-heading' and header.a:
             foundText = header.a.get_text().replace("/n", " ").strip()
             print(foundText)
-            #print(count)
             count+=1
-
 
 #### Problem 2 ####
 print('\n*********** PROBLEM 2 ***********')
@@ -38,7 +31,15 @@ print('Michigan Daily -- MOST READ\n')
 
 ### Your Problem 2 solution goes here
 
+siteHandle = "http://michigandaily.com"
+html=urlopen(siteHandle, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
 
+for section in soup.find_all('div'):
+    if section.has_attr('class'):
+        classList = section['class']
+        if 'view-most-read' in classList:
+            print(section.get_text().replace("/n", " ").strip())
 
 #### Problem 3 ####
 print('\n*********** PROBLEM 3 ***********')
