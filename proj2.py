990b1156e8341ec86eb1d8763fdbc1407f1d833c
kauwers <kauwers@umich.edu>
2017-02-09 14:10:07 -0500

    Q1 complete

--- proj2.py
+++ proj2.py
@@ -2,10 +2,34 @@
 
 
 #### Problem 1 ####
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl, urllib.error, urllib.parse
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
 print('\n*********** PROBLEM 1 ***********')
 print('New York Times -- First 10 Story Headings\n')
 
 ### Your Problem 1 solution goes here
+
+siteHandle = "http://nytimes.com"
+
+html=urlopen(siteHandle, context=ctx).read()
+
+soup = BeautifulSoup(html, 'html.parser')
+
+count = 0
+for header in soup.find_all('h2'):
+    if count < 10:
+        if header.has_attr('class') and header['class'][0] == 'story-heading' and header.a:
+            foundText = header.a.get_text().replace("/n", " ").strip()
+            print(foundText)
+            #print(count)
+            count+=1
 
 
 #### Problem 2 ####
@@ -13,6 +37,7 @@ print('\n*********** PROBLEM 2 ***********')
 print('Michigan Daily -- MOST READ\n')
 
 ### Your Problem 2 solution goes here
+
 
 
 #### Problem 3 ####
